658e52e98aed535156020d5e80363c8678c416e6
gudipatisaicha <chandugudipati230@gmail.com>
2023-10-18 09:52:37 +0530

    Adding spaces

--- Soting/src/BubbleSort/BubbleSort.java
+++ Soting/src/BubbleSort/BubbleSort.java
@@ -8,11 +8,13 @@ public class BubbleSort {
 		int[] a = { 73, 56, 24, 54, 89, 12, 8, 4 };
 		int n = (a.length) - 1;
 		for (int i = 0; i < a.length; i++) {
+			
 			System.out.print(a[i] + "\t");
 		}
 		System.out.println("\n");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n - i; j++) {
+				
 				if (a[j] > a[j + 1]) {
 					int temp = a[j];
 					a[j] = a[j + 1];
@@ -21,6 +23,7 @@ public class BubbleSort {
 			}
 		}
 		for (int i = 0; i < a.length; i++) {
+			
 			System.out.print(a[i] + "\t");
 
 		}
