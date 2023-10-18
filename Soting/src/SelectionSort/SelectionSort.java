e708166c60104ec3b61be68e059c3fef4b63f1a6
gudipatisaicha <chandugudipati230@gmail.com>
2023-10-18 09:46:48 +0530

    Sai changes adding some spaces

--- Soting/src/SelectionSort/SelectionSort.java
+++ Soting/src/SelectionSort/SelectionSort.java
@@ -3,11 +3,12 @@ package SelectionSort;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] a = { 73, 56, 24, 54, 89, 12, 8, 4 };
 		for (int i = 0; i < a.length; i++) {
 			int mindex = i;
+
 			for (int j = i + 1; j < a.length; j++) {
+
 				if (a[mindex] > a[j])
 					mindex = j;
 			}
